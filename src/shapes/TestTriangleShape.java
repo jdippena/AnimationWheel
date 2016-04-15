9f47fbd83a70177e93cde8fc9226d88496a3de88
jdippena <jdippena@macalester.edu>
2016-04-15 01:34:39 -0500

    Added some math functions and tested code to project test triangle onto canvas

---
+++ src/shapes/TestTriangleShape.java
@@ -0,0 +1,14 @@
+package shapes;
+
+import java.util.ArrayList;
+
+/**
+ * Created by Jaco on 4/15/16
+ */
+public class TestTriangleShape extends AbstractShape {
+
+    public TestTriangleShape(float[][] points) {
+        super(new ArrayList<>(1), 0x660000);
+
+    }
+}
