44d8d21ba111f662c575f2d92fcd3d7fabd85259
jdippena <jdippena@macalester.edu>
2016-04-20 21:53:15 -0500

    Migrated the system from Swing to JavaFX, and it's much faster (and simpler) now

--- src/Main.java
+++ src/Main.java
@@ -1,85 +1,130 @@
-import base.*;
+import base.Mat;
+import base.Matrix;
+import javafx.animation.AnimationTimer;
+import javafx.application.Application;
+import javafx.event.EventHandler;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.paint.Color;
+import javafx.stage.Stage;
+import shapes.AbstractShape;
 import shapes.Tetrahedron;
 
-import javax.swing.*;
-import java.awt.*;
-import java.awt.image.BufferedImage;
+import java.util.ArrayList;
 
-public class Main {
+public class Main extends Application implements EventHandler<KeyEvent> {
+    private int width = 800;
+    private int height = 600;
+    private long last;
+
+    Camera camera = new Camera();
+    ArrayList<AbstractShape> shapes = new ArrayList<>();
+    Matrix.Builder builder = new Matrix.Builder();
+    private float angle = 0;
 
     public static void main(String[] args) throws InterruptedException {
-        // Setup the frame
-        Frame frame = new Frame();
-        // Set size
-        frame.setSize(800,600);
-        // Set position to middle of screen
-        Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
-        int width = (int) screenSize.getWidth();
-        int height = (int) screenSize.getHeight();
-        frame.setLocation(width/2-frame.getWidth()/2,height/2-frame.getHeight()/2);
-        // Exit on close
-        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-        // [OFF] Remove borders
-        frame.setUndecorated(false);
-        // Set to be resizeable
-        frame.setResizable(true);
-        // [OFF] Always on top
-        frame.setAlwaysOnTop(false);
-        // Set background color
-        frame.setBackground(Color.white);
-        // Set visible
-        frame.setVisible(true);
+        launch(args);
+    }
 
-        BufferedImage buffer = new BufferedImage(frame.getWidth(), frame.getHeight(), BufferedImage.TYPE_INT_ARGB);
-        Graphics2D image = buffer.createGraphics();
-        frame.paint(image);
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        primaryStage.setTitle("AnimationWheel");
+        Group root = new Group();
+        Scene scene = new Scene(root);
+        scene.setOnKeyPressed(this);
+        primaryStage.setScene(scene);
 
-        /* Old test triangles
-        float[][] points = new float[][] {{0,100,-100,1}, {-100,-50,-100,1}, {100,-50,-100,1}}; // column-major order
-        //frame.addShape(new TestTriangleShape(points));
-
-        points = new float[][] {{0,200,-1000,1}, {-100,50,-1000,1}, {100,50,-1000,1}};
-        //frame.addShape(new TestTriangleShape(points));
-        //*/
+        Canvas canvas = new Canvas(width, height);
+        GraphicsContext context = canvas.getGraphicsContext2D();
+        root.getChildren().add(canvas);
 
         Tetrahedron t = new Tetrahedron();
-        t.setTransform(new Matrix.Builder()
+        shapes.add(t);
+
+        last = System.nanoTime();
+
+        new AnimationTimer() {
+            @Override
+            public void handle(long now) {
+                step(context, now);
+            }
+        }.start();
+
+        primaryStage.show();
+    }
+
+    private void step(GraphicsContext context, long time) {
+        // clear canvas to draw again
+        context.clearRect(0, 0, width, height);
+        float[] facing= Mat.normalize(camera.at);
+
+        angle = (angle + 1f*(time-last)/(16.66666f*1000000)) % 360;
+        float[][] tetrahedronMatrix = builder
                 .scale(20)
-                .translate(0, 0, -100)
-                .build());
-        frame.addShape(t);
+                .translate(0,0,-100)
+                .rotateY(angle)
+                .build();
+        shapes.get(0).setTransform(tetrahedronMatrix);
 
-        boolean persist = true;
-        boolean up=true;
+        for (AbstractShape s : shapes) {
+            for (base.Triangle t : s.mesh) {
+                float[][] points = camera.look(Mat.matrixPointMult(s.transform, t.points));
 
-        //float cameraBob = 1.5F; // Camera y change per frame
-        //float cameraMax = 15; // Camera y bounds (-max to max)
-        while(persist){
-            // Do stuff
+                // Cull tris facing away (which solves depth-checking for simple shapes)
+                float[] edge1= Mat.subtract(points[0], points[1]);
+                float[] edge2= Mat.subtract(points[0], points[2]);
+                float[] norm = Mat.cross(edge1, edge2);
 
-            //frame.paint(image);
-
-            frame.repaint();
-            Thread.sleep(100); // fewer frames works better for some reason???
-
-            // All this animation should probably go into a method in the frame itself
-            // Some sort of step() method maybe?
-
-            frame.modifyAngle(15f); // Rotate over time
-            /*if(up){
-                float[] newCam=frame.getCameraPos();
-                newCam[1]+=cameraBob;
-                if(newCam[1]>cameraMax)
-                    up=false;
-            } else {
-                float[] newCam=frame.getCameraPos();
-                newCam[1]-=cameraBob;
-                if(newCam[1]<-cameraMax)
-                    up=true;
-            }*/
-
-
+                if(Mat.dot(norm, facing)<0) {
+                    double[][] coords = pointsToDisplayPoints(points);
+                    context.setFill(Color.rgb((t.color&0xff0000)>>16, (t.color&0x00ff00)>>8, t.color&0x0000ff));
+                    context.fillPolygon(coords[0], coords[1], 3);
+                }
+            }
         }
+        last = time;
+    }
+
+    @Override
+    public void handle(KeyEvent event) {
+        switch (event.getCode()) {
+            case W:
+                camera.moveZ(-5);
+                break;
+            case S:
+                camera.moveZ(5);
+                break;
+            case D:
+                camera.moveX(5);
+                break;
+            case A:
+                camera.moveX(-5);
+                break;
+            case R:
+                camera.rotateYBy(2);
+                break;
+            case Q:
+                camera.rotateYBy(-2);
+                break;
+        }
+    }
+
+    /**
+     * @param points Points of a triangle in [-1,1] ready to be painted
+     * @return A worldView with two rows, one for x values, the other for y values
+     */
+    private double[][] pointsToDisplayPoints(float [][] points) {
+        int midHeight = height/2;
+        int midWidth = width/2;
+        double[][] newPoints = new double[2][points.length];
+        for (int i = 0; i < points.length; i++) {
+            newPoints[0][i] = (points[i][0]*width) + midWidth; // move and set x
+            newPoints[1][i] = midHeight - (points[i][1]*height); // move and set y
+        }
+        return newPoints;
     }
 
 }
